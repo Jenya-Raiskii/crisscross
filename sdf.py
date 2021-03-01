d8e55f74dc145ae97ad0ab24a9025f39a76ea687
Jenya-Raiskii <79804545+Jenya-Raiskii@users.noreply.github.com>
2021-03-01 17:57:52 +0300

    Update sdf.py

--- sdf.py
+++ sdf.py
@@ -4,6 +4,7 @@ a ='O'
 i=1 
 game_count=0
 count_check=False
+ab=False
 def new():
 	global i, a
 	b=['O','X']
@@ -11,51 +12,67 @@ def new():
 	i+=1
 	if i==2:
 		i=0
+	Lab2['text']="Ход: ",a
 	return a
 
 def ca(event):
 	global a , count_check
-	event.widget['text']=a
+	if event.widget['text']==' ':
+		event.widget['text']=a
 	new()
 	reset_on()
 def obnul():
-		but1["text"]=' '
-		but2["text"]=' '
-		but3["text"]=' '
-		but4["text"]=' '
-		but5["text"]=' '
-		but6["text"]=' '
-		but7["text"]=' '
-		but8["text"]=' '
-		but9["text"]=' '
+	global a
+	but1["text"]=' '
+	but2["text"]=' '
+	but3["text"]=' '
+	but4["text"]=' '
+	but5["text"]=' '
+	but6["text"]=' '
+	but7["text"]=' '
+	but8["text"]=' '
+	but9["text"]=' '
 
 def reset_on():
+	global ab, count_check,game_count
 	if count_check==True:
-		if but1.cget('text') == but2.cget('text') and but2.cget('text') == but3.cget('text'):
+		if (but1.cget('text')!= ' ') and but1.cget('text') == but2.cget('text') and but2.cget('text') == but3.cget('text'):
+			ab=True
 			obnul()
-
-		elif but1.cget('text') == but4.cget('text') and but4.cget('text') == but7.cget('text'):
+			game_count+=1
+		elif (but1.cget('text')!= ' ')and but1.cget('text') == but4.cget('text') and but4.cget('text') == but7.cget('text'):
+			ab=True
 			obnul()
-
-		elif but4.cget('text') == but5.cget('text') and but5.cget('text') == but6.cget('text'):
+			game_count+=1
+		elif (but4.cget('text')!= ' ')and but4.cget('text') == but5.cget('text') and but5.cget('text') == but6.cget('text'):
+			ab=True
 			obnul()
-
-		elif but7.cget('text') == but8.cget('text') and but8.cget('text') == but9.cget('text'):
+			game_count+=1
+		elif (but7.cget('text')!= ' ')and but7.cget('text') == but8.cget('text') and but8.cget('text') == but9.cget('text'):
+			ab=True
 			obnul()
-
-		elif but3.cget('text') == but6.cget('text') and but6.cget('text') == but9.cget('text'):
+			game_count+=1
+		elif (but3.cget('text')!= ' ')and but3.cget('text') == but6.cget('text') and but6.cget('text') == but9.cget('text'):
+			ab=True
 			obnul()
-
-		elif but2.cget('text') == but5.cget('text') and but5.cget('text') == but8.cget('text'):
+			game_count+=1
+		elif (but2.cget('text')!= ' ')and but2.cget('text') == but5.cget('text') and but5.cget('text') == but8.cget('text'):
+			ab=True
 			obnul()
-
-		elif but1.cget('text') == but5.cget('text') and but5.cget('text') == but9.cget('text'):
+			game_count+=1
+		elif (but1.cget('text')!= ' ')and but1.cget('text') == but5.cget('text') and but5.cget('text') == but9.cget('text'):
+			ab=True
 			obnul()
-		elif but1.cget('text') == but4.cget('text') and but4.cget('text') == but7.cget('text'):
+			game_count+=1
+		elif (but1.cget('text')!= ' ')and but1.cget('text') == but4.cget('text') and but4.cget('text') == but7.cget('text'):
+			ab=True
 			obnul()
-
-		elif but3.cget('text') == but5.cget('text') and but5.cget('text') == but7.cget('text'):
+			game_count+=1
+		elif (but3.cget('text')!= ' ')and but3.cget('text') == but5.cget('text') and but5.cget('text') == but7.cget('text'):
+			ab=True
 			obnul()
+			game_count+=1
+		return ab
 
 def reset_off(e):	
 	global game_count		
@@ -66,7 +83,7 @@ def reset_off(e):
 def cheker_on():
 	global count_check, game_count
 	messagebox.showinfo(title="Отключено разработчиком", message="Отключено разработчиком")
-#	count_check=True
+	count_check=True
 def cheker_off():
 	global count_check
 	messagebox.showinfo(title="АвтоПроверка", message="Успешно выключена")
@@ -119,7 +136,7 @@ but_reset= Button(root,width=5,height=5,text='Reset')
 but_reset.bind('<Button-1>',reset_off)
 
 Lab=Label(root, width=10,text='Игра: ',font='ArialBlack 10')
-Lab2=Label(root, width=10,text='Выиграл: ',font='ArialBlack 10')
+Lab2=Label(root, width=10,text='Ход:  ',font='ArialBlack 10')
 
 
 but1.grid(row=1,column=1)
